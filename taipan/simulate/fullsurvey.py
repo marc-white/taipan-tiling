61b26bdd113dfb5924fae596037e6784e83adbfb
= <marc.white@anu.edu.au>
2016-07-12 14:58:24 +1000

    Added scores_array logging

--- taipan/simulate/fullsurvey.py
+++ taipan/simulate/fullsurvey.py
@@ -147,6 +147,8 @@ def sim_do_night(cursor, date, date_start, date_end,
     # Needs to do the following:
     # Read in the tiles that are awaiting observation, along with their scores
     scores_array = rTSexec(cursor, metrics=['cw_sum', 'n_sci_rem'])
+    logging.debug('Scores array info:')
+    logging.debug(scores_array)
 
     # Make sure we have an almanac for every field in the scores_array for the
     # correct date
