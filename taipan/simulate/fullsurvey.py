8284cf16de823f10938f62909f8bdaffa74f42f8
= <marc.white@anu.edu.au>
2016-09-16 15:17:13 +1000

    Shortened full survey test to two weeks for speed while I try to improve hours_observable calculation speed

--- taipan/simulate/fullsurvey.py
+++ taipan/simulate/fullsurvey.py
@@ -191,7 +191,8 @@ def sim_do_night(cursor, date, date_start, date_end,
 
     # Needs to do the following:
     # Read in the tiles that are awaiting observation, along with their scores
-    scores_array = rTSexec(cursor, metrics=['cw_sum', 'n_sci_rem'])
+    scores_array = rTSexec(cursor, metrics=['cw_sum', 'n_sci_rem'],
+                           ignore_zeros=True)
     logging.debug('Scores array info:')
     logging.debug(scores_array)
 
@@ -543,5 +544,5 @@ if __name__ == '__main__':
     cursor = conn.cursor()
     # Execute the simulation based on command-line arguments
     logging.debug('Doing execute function')
-    execute(cursor, datetime.date(2016,4,1), datetime.date(2016,6,1),
+    execute(cursor, datetime.date(2016,4,1), datetime.date(2016,4,15),
             output_loc='.', prep_db=True)
