4fd282c7f359457673bc7cb9880db08df870aa52
= <marc.white@anu.edu.au>
2017-02-08 12:50:07 +1100

    Final version of per-priority target priority setting - will now attempt to re-arrange to make it more similar to ENT pseudo-code

--- taipan/simulate/logic.py
+++ taipan/simulate/logic.py
@@ -31,7 +31,7 @@ def _set_priority_array_values(priorities, bool_arrays, priority):
 
     # Form the combined boolean mask
     bool_arrays = list(bool_arrays)
-    bool_mask = bool_arrays[0]
+    bool_mask = bool_arrays[0].copy()
     for b in bool_arrays[1:]:
         bool_mask = np.logical_and(bool_mask, b)
 
@@ -41,7 +41,7 @@ def _set_priority_array_values(priorities, bool_arrays, priority):
     return
 
 
-def compute_target_priorities(target_info_array):
+def compute_target_priorities(target_info_array, default_priority=20, ):
     """
     Compute priority values for a list of targets.
 
@@ -55,6 +55,9 @@ def compute_target_priorities(target_info_array):
     target_info_array: numpy.array object (structured)
         A numpy structured array containing the target information, one row for
         each target.
+    default_priority : int
+        A default priority value to use for targets which don't satisfy any
+        of the given criteria. Effectively a minimum priority. Defaults to 20.
 
     Returns
     -------
@@ -62,200 +65,210 @@ def compute_target_priorities(target_info_array):
         A list of priorities, corresponding to the target_info_array provided.
     """
 
-    # The default priority is 10, so let's initialize the priorities array with
-    # this value
+    default_priority = int(default_priority)
+
+    # Initialize the priorities array with the default value
     priorities = np.zeros(target_info_array['target_id'].shape)
-    priorities += 10
+    priorities += default_priority
 
     # We now move through the priority bands from lowest to highest,
     # updating the priorities where applicable
 
     # 50-59: i-selected & low-z targets needing more spectra
-    # NOT IMPLEMENTED FOR SIMULATOR
+    # NOT FULLY IMPLEMENTED FOR SIMULATOR
+    # This line will set any vpec targets without zspec (i.e. ones
+    # identified during the survey) to have an intermediate priority
+    # Those targets with a known zpec will get set further down the
+    # priority cascade
+    _set_priority_array_values(priorities,
+                               [
+                                   target_info_array['is_vpec_target'],
+                               ],
+                               55)
 
     # 60-69: BAO-centric targets w/ at least one observation
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_lrg'],
-                                   target_info_array['col_gi'] > 1.4,
-                                   target_info_array['vivits'] == 5,
+                                   target_info_array['col_gi'] >= 1.4,
+                                   target_info_array['visits'] == 5,
                                ], 60)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_lrg'],
-                                   target_info_array['col_gi'] > 1.4,
-                                   target_info_array['vivits'] == 4,
+                                   target_info_array['col_gi'] >= 1.4,
+                                   target_info_array['visits'] == 4,
                                ], 61)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_lrg'],
-                                   target_info_array['col_gi'] > 1.4,
-                                   target_info_array['vivits'] == 3,
+                                   target_info_array['col_gi'] >= 1.4,
+                                   target_info_array['visits'] == 3,
                                ], 62)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_lrg'],
-                                   target_info_array['col_gi'] > 1.4,
-                                   target_info_array['vivits'] == 2,
+                                   target_info_array['col_gi'] >= 1.4,
+                                   target_info_array['visits'] == 2,
                                ], 63)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_lrg'],
-                                   target_info_array['col_gi'] > 1.4,
-                                   target_info_array['vivits'] == 1,
+                                   target_info_array['col_gi'] >= 1.4,
+                                   target_info_array['visits'] == 1,
                                ], 64)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_nir'],
-                                   target_info_array['col_JK'] > 1.,
+                                   target_info_array['col_JK'] >= 1.,
                                    target_info_array['col_JK'] < 1.2,
-                                   target_info_array['vivits'] == 2,
+                                   target_info_array['visits'] == 2,
                                ], 65)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_nir'],
-                                   target_info_array['col_JK'] > 1.,
+                                   target_info_array['col_JK'] >= 1.,
                                    target_info_array['col_JK'] < 1.2,
-                                   target_info_array['vivits'] == 1,
+                                   target_info_array['visits'] == 1,
                                ], 66)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_nir'],
-                                   target_info_array['col_JK'] > 1.2,
-                                   target_info_array['vivits'] == 3,
+                                   target_info_array['col_JK'] >= 1.2,
+                                   target_info_array['visits'] == 3,
                                ], 67)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_nir'],
-                                   target_info_array['col_JK'] > 1.2,
-                                   target_info_array['vivits'] == 2,
+                                   target_info_array['col_JK'] >= 1.2,
+                                   target_info_array['visits'] == 2,
                                ], 68)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_nir'],
-                                   target_info_array['col_JK'] > 1.2,
-                                   target_info_array['vivits'] == 1,
+                                   target_info_array['col_JK'] >= 1.2,
+                                   target_info_array['visits'] == 1,
                                ], 69)
 
     # 70-79: BAO-centric targets without observations
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_lrg'],
-                                   target_info_array['col_gi'] > 1.4,
+                                   target_info_array['col_gi'] >= 1.4,
                                    target_info_array['col_gi'] < 1.5,
                                ], 70)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_lrg'],
-                                   target_info_array['col_gi'] > 1.5,
+                                   target_info_array['col_gi'] >= 1.5,
                                    target_info_array['col_gi'] < 1.6,
                                ], 71)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_lrg'],
-                                   target_info_array['col_gi'] > 1.6,
+                                   target_info_array['col_gi'] >= 1.6,
                                    target_info_array['col_gi'] < 1.7,
                                ], 72)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_lrg'],
-                                   target_info_array['col_gi'] > 1.7,
+                                   target_info_array['col_gi'] >= 1.7,
                                    target_info_array['col_gi'] < 1.8,
                                ], 73)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_lrg'],
-                                   target_info_array['col_gi'] > 1.8,
+                                   target_info_array['col_gi'] >= 1.8,
                                ], 74)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_nir'],
-                                   target_info_array['col_JK'] > 1.0,
+                                   target_info_array['col_JK'] >= 1.0,
                                    target_info_array['col_JK'] < 1.1,
                                ], 75)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_nir'],
-                                   target_info_array['col_JK'] > 1.1,
+                                   target_info_array['col_JK'] >= 1.1,
                                    target_info_array['col_JK'] < 1.2,
                                ], 76)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_nir'],
-                                   target_info_array['col_JK'] > 1.2,
+                                   target_info_array['col_JK'] >= 1.2,
                                    target_info_array['col_JK'] < 1.3,
                                ], 77)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_nir'],
-                                   target_info_array['col_JK'] > 1.3,
+                                   target_info_array['col_JK'] >= 1.3,
                                    target_info_array['col_JK'] < 1.4,
                                ], 78)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_nir'],
-                                   target_info_array['col_JK'] > 1.4,
+                                   target_info_array['col_JK'] >= 1.4,
                                ], 79)
 
     # 80-89: prior-identified vpec targets
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_vpec_target'],
-                                   target_info_array['zspec'] > 0.09,
+                                   target_info_array['zspec'] >= 0.09,
                                    target_info_array['zspec'] < 0.1,
                                ], 80)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_vpec_target'],
-                                   target_info_array['zspec'] > 0.08,
+                                   target_info_array['zspec'] >= 0.08,
                                    target_info_array['zspec'] < 0.09,
                                ], 81)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_vpec_target'],
-                                   target_info_array['zspec'] > 0.08,
+                                   target_info_array['zspec'] >= 0.08,
                                    target_info_array['zspec'] < 0.07,
                                ], 82)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_vpec_target'],
-                                   target_info_array['zspec'] > 0.07,
+                                   target_info_array['zspec'] >= 0.07,
                                    target_info_array['zspec'] < 0.06,
                                ], 83)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_vpec_target'],
-                                   target_info_array['zspec'] > 0.06,
+                                   target_info_array['zspec'] >= 0.06,
                                    target_info_array['zspec'] < 0.05,
                                ], 84)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_vpec_target'],
-                                   target_info_array['zspec'] > 0.05,
+                                   target_info_array['zspec'] >= 0.05,
                                    target_info_array['zspec'] < 0.04,
                                ], 85)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_vpec_target'],
-                                   target_info_array['zspec'] > 0.04,
+                                   target_info_array['zspec'] >= 0.04,
                                    target_info_array['zspec'] < 0.03,
                                ], 86)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_vpec_target'],
-                                   target_info_array['zspec'] > 0.03,
+                                   target_info_array['zspec'] >= 0.03,
                                    target_info_array['zspec'] < 0.02,
                                ], 87)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_vpec_target'],
-                                   target_info_array['zspec'] > 0.02,
+                                   target_info_array['zspec'] >= 0.02,
                                    target_info_array['zspec'] < 0.01,
                                ], 88)
     _set_priority_array_values(priorities,
                                [
                                    target_info_array['is_vpec_target'],
-                                   target_info_array['zspec'] > 0.01,
+                                   target_info_array['zspec'] >= 0.01,
                                    target_info_array['zspec'] < 0.,
                                ], 89)
 
@@ -285,3 +298,5 @@ def compute_target_priorities(target_info_array):
                                    target_info_array['is_iband'],
                                    target_info_array['visits'] == 0,
                                ], 99)
+
+    return priorities
