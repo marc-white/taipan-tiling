630f6cdd55b151513ec46df7f69b9c2d5a9788b3
= <marc.white@anu.edu.au>
2017-02-09 17:20:30 +1100

    Debug

--- taipan/simulate/logic.py
+++ taipan/simulate/logic.py
@@ -87,10 +87,10 @@ def compute_target_types(target_info_array, prisci=False):
 
     # Work out which of the targets are vpec
     if prisci:
-        tgt_types['is_vpec_target'][
-            target_info_array['is_prisci_vpec_target']] = True
+        tgt_types[
+            target_info_array['is_prisci_vpec_target']]['is_vpec_target'] = True
     else:
-        tgt_types['is_vpec_target'][
+        tgt_types[
             np.logical_or(
                 target_info_array['is_prisci_vpec_target'],
                 np.logical_and(
@@ -100,17 +100,17 @@ def compute_target_types(target_info_array, prisci=False):
                     target_info_array['is_full_vpec_target']
                 )
             )
-        ] = True
+        ]['is_vpec_target'] = True
 
 
     # Work out which of the targets are H0
     # Compute the priorities of the targets
     priorities = compute_target_priorities_tree(target_info_array,
                                                 prisci=prisci)
-    tgt_types['is_h0_target'][
+    tgt_types[
         np.logical_and(priorities >= 60,
                        priorities <= 79)
-    ] = True
+    ]['is_h0_target'] = True
 
     # Work out which of the targets are lowz
     # Currently, none (doesn't really affect anything under current logic)
