702793ace7a90e52ef21b111c4a55279b5867d86
= <marc.white@anu.edu.au>
2017-02-23 10:07:20 +1100

    Update to error catching

--- taipan/simulate/fullsurvey-baseline.py
+++ taipan/simulate/fullsurvey-baseline.py
@@ -196,9 +196,11 @@ if __name__ == '__main__':
     # Override the sys.excepthook behaviour to log any errors
     # http://stackoverflow.com/questions/6234405/logging-uncaught-exceptions-in-python
     def excepthook_override(exctype, value, tb):
-        logging.error(
-            'My Error Information\nType: %s\nValue: %s\nTraceback: %s' %
-            (exctype, value, traceback.print_tb(tb), ))
+        # logging.error(
+        #     'My Error Information\nType: %s\nValue: %s\nTraceback: %s' %
+        #     (exctype, value, traceback.print_tb(tb), ))
+        logging.error('Uncaught error/exception detected',
+                      exctype=(exctype, value, tb))
         # logging.error('Type:', exctype)
         # logging.error('Value:', value)
         # logging.error('Traceback:', tb)
