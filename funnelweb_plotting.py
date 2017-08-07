39fece65df02058f3f09c178ad335dd9f33d8cee
Adam Rains <adam.d.rains@gmail.com>
2017-08-07 15:53:20 +1000

    WIP FunnelWeb plotting updates, mainly code neatening, but also working towards plotting tiling completeness for each magnitude bin.

--- funnelweb_plotting.py
+++ funnelweb_plotting.py
@@ -7,8 +7,18 @@ from collections import OrderedDict
 import matplotlib.pyplot as plt
 
 def plot_tiling(test_tiling, remaining_targets, run_settings):
+    """...
+    
+    Parameters
+    ----------
+    test_tiling:
+    
+    remaining_targets:
+    
+    run_settings:
+    
     """
-    """
+    # Extract required parameters from run_settings dictionary
     run_id = run_settings["run_id"]
     description = run_settings["description"]
     time_to_complete = run_settings["mins_to_complete"]
@@ -18,24 +28,47 @@ def plot_tiling(test_tiling, remaining_targets, run_settings):
     combined_weight = run_settings["combined_weight"]
     ranking_method = run_settings["ranking_method"]
     tiling_method = run_settings["tiling_method"]
-    #non_standard_targets_per_tile = run_settings["non_standard_targets_per_tile"]
-    #targets_per_tile = run_settings["targets_per_tile"]
-    #standards_per_tile = run_settings["targets_per_tile"]
-    #guides_per_tile = run_settings["targets_per_tile"]
-    non_standard_targets_per_tile = [t.count_assigned_targets_science(include_science_standards=False) for t in test_tiling]
-    targets_per_tile = [t.count_assigned_targets_science() for t in test_tiling]
-    standards_per_tile = [t.count_assigned_targets_standard() for t in test_tiling]
-    guides_per_tile = [t.count_assigned_targets_guide() for t in test_tiling]
+    non_standard_targets_per_tile = run_settings["non_standard_targets_per_tile"]
+    targets_per_tile = run_settings["targets_per_tile"]
+    standards_per_tile = run_settings["standards_per_tile"]
+    guides_per_tile = run_settings["guides_per_tile"]
+    mag_ranges = [[5,8],[7,10],[9,12],[11,14]]
+    #mag_ranges = run_settings["mag_ranges"]
     
-    
+    # Initialise plot
     plt.clf()
     fig = plt.gcf()
     plt.suptitle(run_id + ": " + description)
     fig.set_size_inches(12., 18.)
-
+    
+    plot_subplot = [True, True, True, True, True, True]
+    
+    # Separate tiles by magnitude range (inefficient)
+    tiles_by_mag_range = []
+    tile_count_labels = ["All"]
+        
+    for mrange in mag_ranges:
+        # Append
+        tiles_for_range = []
+        
+        for tile in test_tiling:
+            if tile.mag_min == mrange[0] and tile.mag_max == mrange[1]:
+                tiles_for_range.append(tile.count_assigned_targets_science())
+        
+        padding = len(test_tiling) - len(tiles_for_range)
+        tiles_by_mag_range.append(np.pad(tiles_for_range, (0, padding), mode="constant",
+            constant_values=-1))
+        tile_count_labels.append("Mag range: " + str(mrange[0]) + "-" + str(mrange[1]))
+    
+    tiles_by_mag_range.insert(0, targets_per_tile)    
+    stacked_tile_counts = np.vstack(tiles_by_mag_range).T
+    
+    
+    # ------------------------------------------------------------------------------------
     # Tile positions
-    ax1 = fig.add_subplot(421, projection='aitoff')
-    ax1.grid(True)
+    # ------------------------------------------------------------------------------------
+    ax0 = fig.add_subplot(421, projection='aitoff')
+    ax0.grid(True)
     # for tile in test_tiling:
         # tile_verts = np.asarray([tp.compute_offset_posn(tile.ra, 
         #     tile.dec, tp.TILE_RADIUS, float(p)) for p in range(361)])
@@ -44,64 +77,72 @@ def plot_tiling(test_tiling, remaining_targets, run_settings):
         # ec = 'k'
         # tile_circ = mpatches.Polygon(tile_verts, closed=False,
         #     edgecolor=ec, facecolor='none', lw=.5)
-        # ax1.add_patch(tile_circ)
-    ax1.plot([np.radians(t.ra - 180.) for t in test_tiling], [np.radians(t.dec) 
+        # ax0.add_patch(tile_circ)
+    ax0.plot([np.radians(t.ra - 180.) for t in test_tiling], [np.radians(t.dec) 
         for t in test_tiling],
         'ko', lw=0, ms=1)
-    ax1.set_title('Tile centre positions')
+    ax0.set_title('Tile centre positions')
 
     #plt.show()
     #plt.draw()
-
+    
+    # ------------------------------------------------------------------------------------
     # Box-and-whisker plots of number distributions
-    ax0 = fig.add_subplot(423)
-    ax0.boxplot([targets_per_tile, standards_per_tile, guides_per_tile],
+    # ------------------------------------------------------------------------------------
+    ax1 = fig.add_subplot(423)
+    ax1.boxplot([targets_per_tile, standards_per_tile, guides_per_tile],
         vert=False)
-    ax0.set_yticklabels(['T', 'S', 'G'])
-    ax0.set_title('Box-and-whisker plots of number of assignments')
+    ax1.set_yticklabels(['T', 'S', 'G'])
+    ax1.set_title('Box-and-whisker plots of number of assignments')
 
-    #plt.show()
-    #plt.draw()
-
-    # Move towards completeness
-    ax3 = fig.add_subplot(223)
+    # ------------------------------------------------------------------------------------
+    # Number of Tiles vs Completeness
+    # ------------------------------------------------------------------------------------
+    ax2 = fig.add_subplot(223)
     targets_per_tile_sorted = sorted(targets_per_tile, key=lambda x: -1.*x)
     xpts = np.asarray(range(len(targets_per_tile_sorted))) + 1
     ypts = [np.sum(targets_per_tile_sorted[:i+1]) for i in xpts]
-    ax3.plot(xpts, ypts, 'k-', lw=.9)
-    ax3.plot(len(test_tiling), np.sum(targets_per_tile), 'ro',
+    ax2.plot(xpts, ypts, 'k-', lw=.9)
+    ax2.plot(len(test_tiling), np.sum(targets_per_tile), 'ro',
         label='No. of tiles: %d' % len(test_tiling))
-    ax3.hlines(num_targets, ax3.get_xlim()[0], ax3.get_xlim()[1], lw=.75,
+    ax2.hlines(num_targets, ax2.get_xlim()[0], ax2.get_xlim()[1], lw=.75,
         colors='k', linestyles='dashed', label='100% completion')
-    ax3.hlines(0.975 * num_targets, ax3.get_xlim()[0], ax3.get_xlim()[1], 
+    ax2.hlines(0.975 * num_targets, ax2.get_xlim()[0], ax2.get_xlim()[1], 
         lw=.75,
         colors='k', linestyles='dashdot', label='97.5% completion')
-    ax3.hlines(0.95 * num_targets, ax3.get_xlim()[0], ax3.get_xlim()[1], lw=.75,
+    ax2.hlines(0.95 * num_targets, ax2.get_xlim()[0], ax2.get_xlim()[1], lw=.75,
         colors='k', linestyles='dotted', label='95% completion')
-    ax3.legend(loc='lower right', title='Time to %3.1f comp.: %dm:%2.1fs' % (
+    ax2.legend(loc='lower right', title='Time to %3.1f comp.: %dm:%2.1fs' % (
         completeness_target * 100., 
         int(np.floor(time_to_complete/60.)), time_to_complete % 60.))
-    ax3.set_title('Completeness progression')
-    ax3.set_xlabel('No. of tiles')
-    ax3.set_ylabel('No. of assigned targets')
+    ax2.set_title('Completeness progression')
+    ax2.set_xlabel('No. of tiles')
+    ax2.set_ylabel('No. of assigned targets')
 
-    #plt.show()
-    #plt.draw()
-
-    # No. of targets per tile
+    # ------------------------------------------------------------------------------------
+    # Number of Targets per Tile
+    # ------------------------------------------------------------------------------------
     target_range = max(targets_per_tile) - min(targets_per_tile)
-    ax2 = fig.add_subplot(322)
-    ax2.hist(targets_per_tile, bins=target_range, align='right')
-    ax2.vlines(tp.TARGET_PER_TILE, ax2.get_ylim()[0], ax2.get_ylim()[1], linestyles='dashed',
+    print min(targets_per_tile)
+    ax3 = fig.add_subplot(322)
+
+    ax3.hist(stacked_tile_counts, bins=np.arange(130), align='right', label=tile_count_labels)
+    
+    ax3.vlines(tp.TARGET_PER_TILE, ax3.get_ylim()[0], ax3.get_ylim()[1], linestyles='dashed',
         colors='k', label='Ideally-filled tile')
-    ax2.legend(loc='upper right')
-    ax2.set_xlabel('No. of targets per tile')
-    ax2.set_ylabel('Frequency')
+    ax3.legend(loc='best')
+    ax3.set_xlabel('No. of targets per tile')
+    ax3.set_ylabel('Frequency')
+    ax3.set_yscale('log')
+    
+    #ax3.hist(targets_per_tile, bins=target_range, alpha=0.5, align='right', label="All")
+    #for i, mrange in enumerate(mag_ranges):
+        #leg_label = "Mag range: " + str(mrange[0]) + "-" + str(mrange[1])
+        #ax3.hist(tiles_by_mag_range[i], bins=target_range, alpha=0.5, align='right', label=leg_label)
 
-    #plt.show()
-    #plt.draw()
-
-    # No. of standards per tile
+    # ------------------------------------------------------------------------------------
+    # Number of standards per tile
+    # ------------------------------------------------------------------------------------
     standard_range = max(standards_per_tile) - min(standards_per_tile)
     if standard_range > 0:
         ax4 = fig.add_subplot(324)
@@ -116,10 +157,9 @@ def plot_tiling(test_tiling, remaining_targets, run_settings):
         ax4.set_ylabel('Frequency')
         ax4.legend(loc='upper left')
 
-    #plt.show()
-    #plt.draw()
-
-    # No. of guides per tile
+    # ------------------------------------------------------------------------------------
+    # Number of guides per tile
+    # ------------------------------------------------------------------------------------
     guide_range = max(guides_per_tile) - min(guides_per_tile)
 
     ax5 = fig.add_subplot(326)
@@ -134,22 +174,20 @@ def plot_tiling(test_tiling, remaining_targets, run_settings):
     ax5.set_ylabel('Frequency')
     ax5.legend(loc='upper left')
 
-    #plt.show()
-    #plt.draw()
 
     supp_str = ''
     if alloc_method == 'combined_weighted':
         supp_str = str(combined_weight)
     elif alloc_method == 'sequential':
         supp_str = str(sequential_ordering)
-    ax2.set_title('GREEDY %s - %d targets, %s tiling, %s unpicking (%s)' % (
+    ax3.set_title('GREEDY %s - %d targets, %s tiling, %s unpicking (%s)' % (
         ranking_method,num_targets,
         tiling_method, alloc_method, supp_str))
     try:
         plt.tight_layout()
     except:
         pass
-
+    
     plt.show()
     plt.draw()
 
