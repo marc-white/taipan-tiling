fcecd0e678f99eb62a86d8748927fd0190a9879d
= <marc.white@anu.edu.au>
2017-10-26 14:55:00 +1100

    Use new ignore_empty functionality in rTS in select_best_tile

--- taipan/simulate/fullsurvey.py
+++ taipan/simulate/fullsurvey.py
@@ -504,7 +504,8 @@ def select_best_tile(cursor, dt, per_end,
     logging.info('Fetching scores array...')
     scores_array = rTSexec(cursor, metrics=['cw_sum', 'prior_sum',
                                             'n_sci_rem'],
-                           ignore_zeros=True, unobserved_only=True)
+                           ignore_zeros=False, unobserved_only=True,
+                           ignore_empty=True)
     logging.info('-- Number of waiting tiles: %d' % len(scores_array))
     scores_array.sort(order='field_id')
 
